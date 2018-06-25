0e1fa70fa679d906487c622c508a3667287d2d7c
apmorris <alice.morris@me.com>
2018-06-25 20:05:51 +0000

    starting to tune face rec

--- camera.py
+++ camera.py
@@ -1,9 +1,11 @@
 from picamera import PiCamera
 from time import sleep
 
+name = raw_input('Enter name: ')
+
 camera = PiCamera()
 
 camera.start_preview()
-sleep(2)
-camera.capture('~/dlib/face_recognition/examples/yuval.jpg')
+sleep(5)
+camera.capture('/home/pi/FacialRecognition/face_rec_ex/'+str(name)+'.jpg')
 camera.stop_preview()
--- face_rec_ex/recognize_faces_in_pictures.py
+++ face_rec_ex/recognize_faces_in_pictures.py
@@ -1,9 +1,9 @@
 import face_recognition
 
 # Load the jpg files into numpy arrays
-biden_image = face_recognition.load_image_file("biden.jpg")
+biden_image = face_recognition.load_image_file("Rhys.jpg")
 obama_image = face_recognition.load_image_file("obama.jpg")
-unknown_image = face_recognition.load_image_file("obama2.jpg")
+unknown_image = face_recognition.load_image_file("Rhys2.jpg")
 
 # Get the face encodings for each face in each image file
 # Since there could be more than one face in each image, it returns a list of encodings.
@@ -22,8 +22,8 @@ known_faces = [
 ]
 
 # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
-results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+results = face_recognition.compare_faces(known_faces, unknown_face_encoding, tolerance=0.5)
 
-print("Is the unknown face a picture of Biden? {}".format(results[0]))
+print("Is the unknown face a picture of Rhys? {}".format(results[0]))
 print("Is the unknown face a picture of Obama? {}".format(results[1]))
 print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
