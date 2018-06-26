d3e057cc0c15cc7bc4889839fb870aa1bc6cf556
apmorris <alice.morris@cern.ch>
2018-06-26 11:57:29 +0100

    testing recognition

--- see_face.py
+++ see_face.py
@@ -14,7 +14,7 @@ def see_face(image):
     return(did_see, locations)
 
 
-def recognise_face_from_locations(image,locations,known_encodings,tolerance):
+def recognise_face_from_locations(image,locations,known_encodings,toleranceLevel):
         # Print the location of each face in this image
         # top, right, bottom, left = location
         # print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
@@ -23,19 +23,23 @@ def recognise_face_from_locations(image,locations,known_encodings,tolerance):
         for encoding in encodings:
             # Loop over each face found in the frame to see if it's someone we know.
             # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([known_encodings[0]], encoding)
-            name = "<Unknown Person>"
-            if match[0]:
-                name = "Barack Obama"
-            print("I see someone named {}!".format(name))
+            friends = 0
+            for key in known_encodings:
+                match = face_recognition.compare_faces([known_encodings[key]], encoding, tolerance=toleranceLevel)
+                if match[0]:
+                    friends += 1
+                    print("I see someone named {}!".format(key))
 
 
 obama_image = face_recognition.load_image_file("face_rec_ex/obama_small.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-known_encodings = [obama_face_encoding]
+alice_image = face_recognition.load_image_file("face_rec_ex/images/Alice3.jpg")
+alice_face_encoding = face_recognition.face_encodings(alice_image)[0]
+known_encodings = { "Barack Obama":obama_face_encoding,
+                    "Alice Morris":alice_face_encoding }
 
 # Load the jpg file into a numpy array
-image = face_recognition.load_image_file("face_rec_ex/two_people.jpg")
+image = face_recognition.load_image_file("face_rec_ex/images/Alice1.jpg")
 
 did_see, locations = see_face(image)
 print(did_see)
