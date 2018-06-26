c457fbe8b023c78d53e1c582f21de85d8195033e
apmorris <alice.morris@cern.ch>
2018-06-26 12:01:42 +0100

    testing recognition

--- see_face.py
+++ see_face.py
@@ -28,7 +28,7 @@ def recognise_face_from_locations(image,locations,known_encodings,toleranceLevel
                 match = face_recognition.compare_faces([known_encodings[key]], encoding, tolerance=toleranceLevel)
                 if match[0]:
                     friends += 1
-                    print("I see someone named {}!".format(key))
+                    print("I found {}!".format(key))
             return friends
 
 obama_image = face_recognition.load_image_file("face_rec_ex/obama_small.jpg")
@@ -44,4 +44,4 @@ image = face_recognition.load_image_file("face_rec_ex/two_people.jpg")
 did_see, locations = see_face(image)
 print(did_see)
 nFriends = recognise_face_from_locations(image,locations,known_encodings,0.5)
-print('I have '+nFriends+' friends')
+print('I have '+str(nFriends)+' friends')
