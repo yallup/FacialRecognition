f779e0ba1999de8d1c2bcc0477e251885c6f0081
apmorris <alice.morris@cern.ch>
2018-06-26 12:04:30 +0100

    testing recognition

--- see_face.py
+++ see_face.py
@@ -19,17 +19,16 @@ def recognise_face_from_locations(image,locations,known_encodings,toleranceLevel
         # top, right, bottom, left = location
         # print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
         encodings = face_recognition.face_encodings(image, locations)
-
+        friends = 0
         for encoding in encodings:
             # Loop over each face found in the frame to see if it's someone we know.
             # See if the face is a match for the known face(s)
-            friends = 0
             for key in known_encodings:
                 match = face_recognition.compare_faces([known_encodings[key]], encoding, tolerance=toleranceLevel)
                 if match[0]:
                     friends += 1
                     print("I found {}!".format(key))
-            return friends
+        return friends
 
 obama_image = face_recognition.load_image_file("face_rec_ex/obama_small.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
@@ -43,5 +42,5 @@ image = face_recognition.load_image_file("face_rec_ex/two_people.jpg")
 
 did_see, locations = see_face(image)
 print(did_see)
-nFriends = recognise_face_from_locations(image,locations,known_encodings,0.5)
+nFriends = recognise_face_from_locations(image,locations,known_encodings,0.55)
 print('I have '+str(nFriends)+' friends')
